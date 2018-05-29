243b897740e635599602c013b28f3ea89ff1d5b7
lukacupic <lukacupic42@gmail.com>
2018-05-29 08:46:28 +0200

    Add feature to open a document once clicked on the graph vertex

--- src/main/java/hr/fer/zemris/zavrsni/VisualizationDemo.java
+++ src/main/java/hr/fer/zemris/zavrsni/VisualizationDemo.java
@@ -1,26 +1,32 @@
 package hr.fer.zemris.zavrsni;
 
-import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout2;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.GraphMouseListener;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import hr.fer.zemris.zavrsni.model.Document;
 import hr.fer.zemris.zavrsni.ranking.RankingFunction;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import java.awt.*;
+import java.awt.event.*;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Luka Cupic
  */
 public class VisualizationDemo {
 
-	private static final double threshold = 0.08;
+	private static final double threshold = 0.07;
 
 	public static void main(String[] args) throws IOException {
 		Main.init(Paths.get(args[0]));
@@ -42,19 +48,17 @@ public class VisualizationDemo {
 			}
 		}
 
-		FRLayout<Document, String> layout = new FRLayout<>(g);
-		//SpringLayout<Integer, Edge> layout = new SpringLayout<>(g, e -> e.weight);
+		FRLayout2<Document, String> layout = new FRLayout2<>(g);
 		layout.setSize(new Dimension(600, 600));
 		layout.initialize();
 
-		int count = 0;
-		while (!layout.done() && count < 700) {
+		layout.setRepulsionMultiplier(0.8);
+
+		while (!layout.done()) {
 			layout.step();
-			count++;
 		}
 
 		VisualizationViewer<Document, String> vv = new VisualizationViewer<>(layout);
-
 		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller() {
 			@Override
 			public String apply(Object v) {
@@ -62,11 +66,37 @@ public class VisualizationDemo {
 			}
 		});
 
+		Function<Document, Paint> vertexColor = d -> new Color(9, 67, 138, 188);
+		vv.getRenderContext().setVertexFillPaintTransformer(vertexColor::apply);
+
+		vv.addGraphMouseListener(new GraphMouseListener<Document>() {
+			@Override
+			public void graphClicked(Document d, MouseEvent me) {
+				if (SwingUtilities.isLeftMouseButton(me)) {
+					try {
+						Desktop.getDesktop().open(d.getPath().toFile());
+					} catch (IOException e) {
+					}
+				}
+				me.consume();
+			}
+
+			@Override
+			public void graphPressed(Document document, MouseEvent me) {
+			}
+
+			@Override
+			public void graphReleased(Document document, MouseEvent me) {
+			}
+		});
+
+		DefaultModalGraphMouse graphMouse = new DefaultModalGraphMouse();
+		graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
+		vv.setGraphMouse(graphMouse);
+
 		JFrame frame = new JFrame("Simple Graph View");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.getContentPane().
-
-				add(vv);
+		frame.getContentPane().add(vv);
 		frame.pack();
 		frame.setVisible(true);
 	}
